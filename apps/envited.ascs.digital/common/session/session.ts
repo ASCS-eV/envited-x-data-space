dd35abde04ee7e654bd822177f333208348ebd96
Roy Scheeren <hi@royscheeren.com>
2024-01-11 10:55:58 +0100

    test(envited.ascs.digital): add test for server session
    Signed-off-by: Roy Scheeren <hi@royscheeren.com>

--- apps/envited.ascs.digital/common/session/session.ts
+++ apps/envited.ascs.digital/common/session/session.ts
@@ -1,7 +1,7 @@
-import { getServerSession as NAGetServerSession } from 'next-auth'
+import { getServerSession as NAGetServerSession, NextAuthOptions } from 'next-auth'
 
 import { authOptions } from '../../app/api/auth/[...nextauth]/route'
 
-export const _getServerSession = (NAGetServerSession: any) => () => NAGetServerSession(authOptions)
+export const _getServerSession = (NAGetServerSession: any) => (authOptions: NextAuthOptions) => () => NAGetServerSession(authOptions)
 
-export const getServerSession = _getServerSession(NAGetServerSession)
+export const getServerSession = _getServerSession(NAGetServerSession)(authOptions)

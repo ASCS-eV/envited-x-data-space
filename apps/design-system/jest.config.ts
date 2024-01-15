c1f9fbbdc15bddbac7a9afaebf7a3eeeee8efb4a
Roy Scheeren <hi@royscheeren.com>
2024-01-15 14:13:29 +0100

    fix(design-system): timezone differences
    Signed-off-by: Roy Scheeren <hi@royscheeren.com>

--- apps/design-system/jest.config.ts
+++ apps/design-system/jest.config.ts
@@ -1,4 +1,5 @@
 /* eslint-disable */
+process.env.TZ = 'GMT'
 export default {
   displayName: 'design-system',
   preset: '../../jest.preset.js',
--- apps/design-system/src/components/Atoms/ThemeToggle/ThemeToggle.ui.test.tsx
+++ apps/design-system/src/components/Atoms/ThemeToggle/ThemeToggle.ui.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 
 import { noop } from '../../../common/utils'
 import { ColorScheme } from '../../../types'

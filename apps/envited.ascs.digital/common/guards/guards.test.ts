5e862ffded22cab3fcf8bb82e2ef1843703311dd
Jeroen Branje <info@jeroenbranje.nl>
2024-01-25 21:50:50 +0100

    feat: add guardss test
    Signed-off-by: Jeroen Branje <info@jeroenbranje.nl>

--- apps/envited.ascs.digital/common/guards/guards.test.ts
+++ apps/envited.ascs.digital/common/guards/guards.test.ts
@@ -1,5 +1,5 @@
 import { USER_CREDENTIAL } from '../../common/fixtures'
-import { Session, User } from '../types'
+import { Role, Session, User } from '../types'
 import * as SUT from './guards'
 
 describe('common/guards', () => {
@@ -40,6 +40,45 @@ describe('common/guards', () => {
       const result = SUT.isOwnUser(user as User)(session as Session)
 
       expect(result).toEqual(true)
+    })
+  })
+
+  describe('isFederator', () => {
+    it.each([
+      [Role.federator, true],
+      [Role.principal, false],
+    ])('should check if logged in user is a federator', (role, expected) => {
+      // when ... we want to check if the user has the federator role
+      const session = {
+        user: {
+          pkh: 'PKH',
+          role,
+        },
+      }
+      // then ... we should get the result as expected
+      const result = SUT.isFederator(session as Session)
+
+      expect(result).toEqual(expected)
+    })
+  })
+
+  describe('isPrincipal', () => {
+    it.each([
+      [Role.federator, false],
+      [Role.principal, true],
+      [Role.user, false],
+    ])('should check if logged in user is a principal', (role, expected) => {
+      // when ... we want to check if the user has the principal
+      const session = {
+        user: {
+          pkh: 'PKH',
+          role,
+        },
+      }
+      // then ... we should get the result as expected
+      const result = SUT.isPrincipal(session as Session)
+
+      expect(result).toEqual(expected)
     })
   })
 })

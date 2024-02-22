316057a293a8edf3d3dcd4df4c0505b3c8c96f66
Roy Scheeren <hi@royscheeren.com>
2024-02-22 13:41:25 +0100

    feat(oidc-client): qr code login page
    Signed-off-by: Roy Scheeren <hi@royscheeren.com>

--- apps/oidc-client/src/App.tsx
+++ apps/oidc-client/src/App.tsx
@@ -4,16 +4,17 @@ function App() {
   return (
     <div class={styles.App}>
       <header class={styles.header}>
-          <div class={styles.logo}>
-            <img src='/envited-logo.png' alt='Envited Data Space' />
-          </div>
-        </header>
+        <div class={styles.logo}>
+          <img src="/envited-logo.png" alt="Envited Data Space" />
+        </div>
+      </header>
       <main class={styles.container}>
-        
-        <p>Sign in with your verified credentials to continue to <strong>Envited Data Space</strong></p>
-        
+        <p>
+          Sign in with your verified credentials to continue to <strong>Envited Data Space</strong>
+        </p>
+
         <div class={`${styles.qrCodeContainer} ${styles.shadow}`}>
-          <img src='/qrcode.png' alt='QR code' />
+          <img src="/qrcode.png" alt="QR code" />
         </div>
       </main>
     </div>

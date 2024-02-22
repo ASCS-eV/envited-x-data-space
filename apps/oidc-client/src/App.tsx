3cfbf06686bc8aa062c1f67147496123416f7881
Roy Scheeren <hi@royscheeren.com>
2024-02-22 15:23:22 +0100

    feat(oidc-client): wip
    Signed-off-by: Roy Scheeren <hi@royscheeren.com>

--- apps/oidc-client/src/App.tsx
+++ apps/oidc-client/src/App.tsx
@@ -1,6 +1,24 @@
+import { useState } from 'react'
+import ReactQRCode from 'react-qr-code'
+
 import styles from './App.module.css'
+import { getOpenIdConnectUrl } from './utils'
+
+const QRCode = ReactQRCode as any
 
 function App() {
+  const urlSearchParams = new URLSearchParams(window.location.search)
+  const [error, setError] = useState<string | null>(null)
+  const [qrCodeValue, setQrCodeValue] = useState<string | null>(null)
+
+  try {
+    const url = getOpenIdConnectUrl(urlSearchParams)
+    setQrCodeValue(url)
+  } catch (e: any) {
+    setQrCodeValue(null)
+    setError(e.message)
+  }
+
   return (
     <div class={styles.App}>
       <header class={styles.header}>
@@ -12,14 +30,17 @@ function App() {
         <p>
           Sign in with your verified credentials to continue to <strong>Envited Data Space</strong>
         </p>
-
-        <div class={`${styles.qrCodeContainer} ${styles.shadow}`}>
-          <img src="/qrcode.png" alt="QR code" />
-        </div>
-        <div class={styles.error}>
-          <h3 class={styles.errorTitle}>Error</h3>
-          <p>Something went wrong</p>
-        </div>
+        {qrCodeValue && (
+          <div class={`${styles.qrCodeContainer} ${styles.shadow}`}>
+            <QRCode value={qrCodeValue} />
+          </div>
+        )}
+        {error && (
+          <div class={styles.error}>
+            <h3 class={styles.errorTitle}>Error</h3>
+            <p>{error}</p>
+          </div>
+        )}
       </main>
     </div>
   )
---
+++ apps/oidc-client/src/utils/index.ts
@@ -0,0 +1,1 @@
+export { getOpenIdConnectUrl } from './utils'

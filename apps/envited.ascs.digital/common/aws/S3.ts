10ec71305570e40b1d8396451d6681ed446f7384
Jeroen Branje <info@jeroenbranje.nl>
2024-02-27 16:21:43 +0100

    feat(envited.ascs.digital): Add S3 test
    Signed-off-by: Jeroen Branje <info@jeroenbranje.nl>

--- apps/envited.ascs.digital/common/aws/S3.ts
+++ apps/envited.ascs.digital/common/aws/S3.ts
@@ -1,29 +1,33 @@
 import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3'
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
 import type { getSignedUrl as TgetSignedUrl } from '@aws-sdk/s3-request-presigner'
+
 import { createRandomString } from '../utils'
 
 const s3Client = new S3Client({})
 const putObjectCommand = PutObjectCommand
+const randomString = createRandomString(5)
 
 export const _getUploadUrl =
   ({
     getSignedUrl,
     s3Client,
     putObjectCommand,
+    randomString,
   }: {
     getSignedUrl: typeof TgetSignedUrl
     s3Client: S3Client
     putObjectCommand: typeof PutObjectCommand
+    randomString: string
   }) =>
   async (slug: string, filename: string) => {
     const command = new putObjectCommand({
       ACL: 'public-read',
-      Key: `${slug}-${createRandomString(5)}-.${filename.split('').pop()}`,
+      Key: `${slug}-${randomString}.${filename.split('.').pop()}`,
       Bucket: process.env.NEXT_PUBLIC_UPLOAD_BUCKET_NAME,
     })
 
     return await getSignedUrl(s3Client, command)
   }
 
-export const getUploadUrl = _getUploadUrl({ getSignedUrl, s3Client, putObjectCommand })
+export const getUploadUrl = _getUploadUrl({ getSignedUrl, s3Client, putObjectCommand, randomString })

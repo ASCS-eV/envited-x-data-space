496bbefb07fe36d13e267ed6d81ca63edbca0ba7
Jeroen Branje <info@jeroenbranje.nl>
2024-02-29 16:23:01 +0100

    feat(envited.ascs.digital): Show offerings on profile
    Signed-off-by: Jeroen Branje <info@jeroenbranje.nl>

--- apps/envited.ascs.digital/modules/Member/Member.tsx
+++ apps/envited.ascs.digital/modules/Member/Member.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { Address, Card, ContactPerson, Heading, Pill } from '@envited-marketplace/design-system'
-import { has, isNil, map, propOr } from 'ramda'
+import { has, isNil, map, prop, propOr } from 'ramda'
 import React, { FC } from 'react'
 
 import { useTranslation } from '../../common/i18n'
@@ -30,6 +30,54 @@ export const Member: FC<MemberProps> = ({ member }) => {
             <div className="flex gap-3 mt-2">{propOr('', 'description')(member)}</div>
           </div>
         </Card>
+        {!isNil(member.offerings) && (
+          <Card>
+            <Heading importance="h4">{t('[Heading] offerings')}</Heading>
+            {map(({ name, type, functionalities, supportedTools, supportedStandards }) => (
+              <div className="mt-4">
+                <div className="px-4 sm:px-0">
+                  <h3 className="text-base font-semibold leading-7 text-gray-900 dark:text-white">{name}</h3>
+                </div>
+                <div className="mt-6 border-t border-gray-100 dark:border-gray-800">
+                  <dl className="divide-y divide-gray-100 dark:divide-gray-800">
+                    <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                      <dt className="text-sm font-medium leading-6 text-gray-900 dark:text-gray-500">
+                        {t('[Heading] offering type')}
+                      </dt>
+                      <dd className="mt-1 text-sm leading-6 text-gray-700 dark:text-white sm:col-span-2 sm:mt-0">
+                        {map((item: string) => <Pill key={item}>{item}</Pill>)(type.split(';') as [])}
+                      </dd>
+                    </div>
+                    <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                      <dt className="text-sm font-medium leading-6 text-gray-900 dark:text-gray-500">
+                        {t('[Heading] offering functionalities')}
+                      </dt>
+                      <dd className="mt-1 text-sm leading-6 text-gray-700 dark:text-white sm:col-span-2 sm:mt-0">
+                        {functionalities}
+                      </dd>
+                    </div>
+                    <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                      <dt className="text-sm font-medium leading-6 text-gray-900 dark:text-gray-500">
+                        {t('[Heading] offering supported tools')}
+                      </dt>
+                      <dd className="mt-1 text-sm leading-6 text-gray-700 dark:text-white sm:col-span-2 sm:mt-0">
+                        {supportedTools}
+                      </dd>
+                    </div>
+                    <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                      <dt className="text-sm font-medium leading-6 text-gray-900 dark:text-gray-500">
+                        {t('[Heading] offering supported standards')}
+                      </dt>
+                      <dd className="mt-1 text-sm leading-6 text-gray-700 dark:text-white sm:col-span-2 sm:mt-0">
+                        {supportedStandards}
+                      </dd>
+                    </div>
+                  </dl>
+                </div>
+              </div>
+            ))(propOr([], 'offerings')(member))}
+          </Card>
+        )}
       </div>
       <div className="w-1/4 flex flex-col gap-8">
         <Card>
@@ -65,7 +113,7 @@ export const Member: FC<MemberProps> = ({ member }) => {
             </div>
           </Card>
         )}
-        {has('salesName')(member) && has('salesEmail')(member) && has('salesPhone')(member) && (
+        {!isNil(prop('salesName')(member)) && has('salesEmail')(member) && has('salesPhone')(member) && (
           <Card>
             <Heading importance="h4">{t('[Heading] sales contact')}</Heading>
             <div className="mt-4">

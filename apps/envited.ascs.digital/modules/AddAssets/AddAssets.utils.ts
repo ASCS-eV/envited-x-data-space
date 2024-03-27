aff7e82e67b6bd3a425a6efe83c34046957302d1
Jeroen Branje <info@jeroenbranje.nl>
2024-03-27 15:40:02 +0100

    feat(envited.ascs.digital): Extract read zip function from validation
    Signed-off-by: Jeroen Branje <info@jeroenbranje.nl>

--- apps/envited.ascs.digital/modules/AddAssets/AddAssets.utils.ts
+++ apps/envited.ascs.digital/modules/AddAssets/AddAssets.utils.ts
@@ -1,5 +1,5 @@
 import { BlobReader, ZipReader } from '@zip.js/zip.js'
-import { equals, find, propEq, times } from 'ramda'
+import { equals, find, isNil, propEq, times } from 'ramda'
 
 import { ASSETS_VALIDATION_MAP, Asset, MetadataSchema } from './AddAssets.schema'
 
@@ -12,30 +12,43 @@ export const removeFileHandler = (files: FileList, idx: number) => {
 
 export const validateAssetFile = async (file: File) => {
   try {
-    const content = await new ZipReader(new BlobReader(file)).getEntries()
-    const metadataStream = new TransformStream()
-    const metadataStreamPromise = new Response(metadataStream.readable).text()
-    const metadataEntry = find(propEq('metadata.json', 'filename'))(content) as any
-
-    await metadataEntry.getData(metadataStream.writable)
-
-    const metadata = await metadataStreamPromise
-    const parsedMetadata = JSON.parse(metadata)
-
-    const metadataResult = MetadataSchema.safeParse(parsedMetadata)
+    const metadata = await getMetadataJsonFromZip(file)
+    const metadataResult = MetadataSchema.safeParse(metadata)
 
     if (!metadataResult.success) {
       return { isValidating: false, isValid: false, data: {} }
     }
 
-    const { type } = MetadataSchema.parse(parsedMetadata)
-    const resultAssetTypeValidation = ASSETS_VALIDATION_MAP[type as Asset].safeParse(parsedMetadata)
+    const { type } = MetadataSchema.parse(metadata)
+    const resultAssetTypeValidation = ASSETS_VALIDATION_MAP[type as Asset].safeParse(metadata)
 
     if (!resultAssetTypeValidation.success) {
       return { isValidating: false, isValid: false, data: {} }
     }
 
-    return { isValidating: false, isValid: true, data: parsedMetadata }
+    return { isValidating: false, isValid: true, data: metadata }
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+export const getMetadataJsonFromZip = async (file: File) => {
+  try {
+    const zipReader = new ZipReader(new BlobReader(file))
+    const zipEntries = await zipReader.getEntries()
+    const metadataEntry = find(propEq('metadata.json', 'filename'))(zipEntries) as any
+
+    if (isNil(metadataEntry)) {
+      return {}
+    }
+
+    const metadataStream = new TransformStream()
+    const metadataStreamPromise = new Response(metadataStream.readable).text()
+    await metadataEntry.getData(metadataStream.writable)
+    const metadata = await metadataStreamPromise
+    await zipReader.close()
+
+    return JSON.parse(metadata)
   } catch (e) {
     console.log(e)
   }

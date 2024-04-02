687d4588e2b54e8ac43ba4f140e56b3ffb716cc4
Jeroen Branje <info@jeroenbranje.nl>
2024-04-02 16:40:23 +0200

    refactor(envited.ascs.digital): remove comment
    Signed-off-by: Jeroen Branje <info@jeroenbranje.nl>

--- apps/envited.ascs.digital/setupTests.ts
+++ apps/envited.ascs.digital/setupTests.ts
@@ -3,8 +3,6 @@ import { TextDecoder, TextEncoder } from 'util'
 
 Object.assign(global, { TextDecoder, TextEncoder })
 
-// Object.assign(global, new TransformStream)
-
 const testCache = <T extends Function>(func: T) => func
 
 jest.mock('react', () => {

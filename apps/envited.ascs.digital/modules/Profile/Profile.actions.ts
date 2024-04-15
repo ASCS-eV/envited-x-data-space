1df9fb1542d5c909cc68a5483573756b4f2f1e6b
Roy Scheeren <hi@royscheeren.com>
2024-04-15 16:55:22 +0200

    test(envited.ascs.digital): test fetch result
    Signed-off-by: Roy Scheeren <hi@royscheeren.com>

--- apps/envited.ascs.digital/modules/Profile/Profile.actions.ts
+++ apps/envited.ascs.digital/modules/Profile/Profile.actions.ts
@@ -37,7 +37,7 @@ export async function updateProfileForm(formData: FormData) {
           'Content-Type': file.type,
           'Content-Disposition': `inline; filename="${file.name}"`,
         },
-      })
+      }).then((res) => res.json())
 
       data = { ...data, logo: image.url.split('?')[0] }
       console.log('data', data, image)

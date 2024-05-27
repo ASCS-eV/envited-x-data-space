8cb1dcfb497ec49f0d20a69d6d7761b7253dd9c2
Jeroen Branje <info@jeroenbranje.nl>
2024-05-27 16:12:42 +0200

    feat(envited.ascs.digital): give lambda permissions to read files
    Signed-off-by: Jeroen Branje <info@jeroenbranje.nl>

--- apps/envited.ascs.digital/stacks/Envited.ts
+++ apps/envited.ascs.digital/stacks/Envited.ts
@@ -86,6 +86,7 @@ export default function Envited({ stack }: StackContext) {
     },
     cors: [s3CorsRule],
   })
+  assetsBucket.attachPermissions([assetsBucket]);
   assetsBucket.cdk.bucket.grantRead(oai)
 
   const assetsDistribution = new aws_cloudfront.CloudFrontWebDistribution(stack, 'assetsDistribution', {

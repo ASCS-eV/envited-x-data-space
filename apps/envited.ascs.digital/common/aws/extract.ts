7b627e6865399b2da60e08defb7812a7efe31079
Jeroen Branje <info@jeroenbranje.nl>
2024-05-27 15:19:39 +0200

    feat(envited.ascs.digital): get filename and bucket from event
    Signed-off-by: Jeroen Branje <info@jeroenbranje.nl>

--- apps/envited.ascs.digital/common/aws/extract.ts
+++ apps/envited.ascs.digital/common/aws/extract.ts
@@ -1,5 +1,18 @@
 import { S3Handler } from 'aws-lambda'
 
+const prefix = `extract`
+
 export const main: S3Handler = async event => {
-  console.log('Extract handler', event)
+  const s3Record = event.Records[0].s3
+
+  const Key = s3Record.object.key
+  const Bucket = s3Record.bucket.name
+
+  if (Key.startsWith(prefix)) {
+    return
+  }
+
+  console.log('/*** Extract handler - Key ***/', Key)
+  console.log('/*** Extract handler - Bucket ***/', Bucket)
+  console.log('/*** Extract handler - S3Record ***/', s3Record)
 }

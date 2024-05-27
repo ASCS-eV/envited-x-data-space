5c2b1ef3b64ddbe5042263509364d9f72a4c096a
Jeroen Branje <info@jeroenbranje.nl>
2024-05-27 15:51:45 +0200

    feat(envited.ascs.digital): get file from S3 and log stream
    Signed-off-by: Jeroen Branje <info@jeroenbranje.nl>

--- apps/envited.ascs.digital/common/aws/extract.ts
+++ apps/envited.ascs.digital/common/aws/extract.ts
@@ -1,6 +1,24 @@
+import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3'
 import { S3Handler } from 'aws-lambda'
 
 const prefix = `extract`
+
+const S3 = new S3Client({})
+
+const readStreamFromS3 = async ({ Bucket, Key }: { Bucket: string; Key: string }) => {
+  try {
+    const commandPullObject = new GetObjectCommand({
+      Bucket,
+      Key,
+    })
+    const response = await S3.send(commandPullObject)
+
+    return response
+  } catch (err) {
+    console.log(err)
+    throw err
+  }
+}
 
 export const main: S3Handler = async event => {
   const s3Record = event.Records[0].s3
@@ -11,6 +29,10 @@ export const main: S3Handler = async event => {
   if (Key.startsWith(prefix)) {
     return
   }
+
+  const readStream = await readStreamFromS3({ Key, Bucket })
+  console.log('/**** ReadStream - Response', readStream)
+  console.log('/**** ReadStream - Body', readStream.Body)
 
   console.log('/*** Extract handler - Key ***/', Key)
   console.log('/*** Extract handler - Bucket ***/', Bucket)

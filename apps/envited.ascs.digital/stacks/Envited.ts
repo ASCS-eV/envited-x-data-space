ba3471def4ec4ac36eeb3ca10f3f12fffcfb6a3a
Jeroen Branje <info@jeroenbranje.nl>
2024-06-04 15:38:48 +0200

    feat(envited.ascs.digital): update schema file path
    Signed-off-by: Jeroen Branje <info@jeroenbranje.nl>

--- apps/envited.ascs.digital/stacks/Envited.ts
+++ apps/envited.ascs.digital/stacks/Envited.ts
@@ -1,4 +1,4 @@
-import { aws_cloudfront, aws_ec2, aws_lambda, aws_rds, aws_s3 } from 'aws-cdk-lib'
+import { aws_cloudfront, aws_ec2, aws_rds, aws_s3 } from 'aws-cdk-lib'
 import { RetentionDays } from 'aws-cdk-lib/aws-logs'
 import * as cdk from 'aws-cdk-lib/core'
 import { Bucket, NextjsSite, StackContext } from 'sst/constructs'
@@ -92,11 +92,6 @@ export default function Envited({ stack }: StackContext) {
           },
           permissions: [metadataBucket],
           copyFiles: [{ from: 'common/aws/validateAndExtractMetadata/schemas' }],
-          // layers: [
-          //   new aws_lambda.LayerVersion(stack, 'Schemas', {
-          //     code: aws_lambda.Code.fromAsset('common/aws/validateAndExtractMetadata/schemas'),
-          //   }),
-          // ],
         },
         events: ['object_created'],
       },

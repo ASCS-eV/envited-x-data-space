3fe00f61587047778eee268f93c48df0eb515a91
Jeroen Branje <info@jeroenbranje.nl>
2024-09-03 16:25:56 +0200

    refactor: use ramda includes
    Signed-off-by: Jeroen Branje <info@jeroenbranje.nl>

--- apps/envited.ascs.digital/middleware.ts
+++ apps/envited.ascs.digital/middleware.ts
@@ -1,5 +1,6 @@
 import { withAuth } from 'next-auth/middleware'
 import { NextResponse } from 'next/server'
+import { includes } from 'ramda'
 
 import { AUTHORIZED_ROLES, ROLE_PROTECTED_ROUTES_MAP } from './common/constants'
 import { ROUTES } from './common/constants/routes'
@@ -8,15 +9,15 @@ import { Role } from './common/types'
 export default withAuth(
   req => {
     const role = req.nextauth.token?.user.role as Role
-    const path = req.nextUrl.pathname
+    const path = req.nextUrl.pathname as string
 
-    if (!ROLE_PROTECTED_ROUTES_MAP[role].includes(path)) {
+    if (!includes(path)(ROLE_PROTECTED_ROUTES_MAP[role])) {
       return NextResponse.redirect(new URL(ROUTES.DASHBOARD.HOME, req.url))
     }
   },
   {
     callbacks: {
-      authorized: ({ token }) => AUTHORIZED_ROLES.includes(token?.user.role),
+      authorized: ({ token }) => includes(token?.user.role as Role)(AUTHORIZED_ROLES),
     },
   },
 )

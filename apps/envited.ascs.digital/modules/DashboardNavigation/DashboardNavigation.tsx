284d528b7cc9460495373f406e7db99e7d3af27e
Jeroen Branje <info@jeroenbranje.nl>
2024-10-15 11:08:57 +0200

    feat: signout button dashboard
    Signed-off-by: Jeroen Branje <info@jeroenbranje.nl>

--- apps/envited.ascs.digital/modules/DashboardNavigation/DashboardNavigation.tsx
+++ apps/envited.ascs.digital/modules/DashboardNavigation/DashboardNavigation.tsx
@@ -42,6 +42,15 @@ export const DashboardNavigation: FC<{ items: NavItemProps[] }> = ({ items }) =>
   return (
     <Nav>
       {map(({ href, name, icon }: NavItemProps) => <NavLink key={href} href={href} name={name} icon={icon} />)(items)}
+      <NavItem>
+        <div className="mt-3 pt-3 w-full border-t border-gray-200">
+          <button onClick={signOut} className={``}>
+            <div className="w-full py-4 px-5">
+              <p className="text-base font-medium">Sign out</p>
+            </div>
+          </button>
+        </div>
+      </NavItem>
     </Nav>
   )
 }

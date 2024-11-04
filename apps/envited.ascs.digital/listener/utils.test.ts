9ca08743b29d7f47f4b8f46bb3ac60a4cd21344a
Roy Scheeren <hi@royscheeren.com>
2024-11-04 09:16:55 +0100

    test(envited.ascs.digital): skip listener tests
    Signed-off-by: Roy Scheeren <hi@royscheeren.com>

--- apps/envited.ascs.digital/listener/utils.test.ts
+++ apps/envited.ascs.digital/listener/utils.test.ts
@@ -1,39 +1,39 @@
-import * as SUT from './utils'
+// import * as SUT from './utils'
 
-describe.skip('listener/utils', () => {
-  describe('extractAttributesUri', () => {
-    it('should return an empty array if the attributes array is empty', () => {
-      // when ... we want to extract the attributes uri
-      // then ... it should extract the attributes uri as expected
+// describe.skip('listener/utils', () => {
+//   describe('extractAttributesUri', () => {
+//     it('should return an empty array if the attributes array is empty', () => {
+//       // when ... we want to extract the attributes uri
+//       // then ... it should extract the attributes uri as expected
 
-      const attributes = [
-        {
-          name: 'ASSET_NAME.ASSET_TYPE',
-          value: 'ASSET_URI',
-          type: 'uri',
-        },
-        {
-          name: 'ASSET_NAME.ASSET_TYPE',
-          value: 'ASSET_IPFS_URI',
-          type: 'application/json',
-        },
-        {
-          name: 'ASSET_NAME.manifest',
-          value: 'ASSET_DEFINITION_URI',
-          type: 'uri',
-        },
-        {
-          name: 'ASSET_NAME.manifest',
-          value: 'ASSET_MANIFEST_IFPS_URI',
-          type: 'application/json',
-        },
-      ]
+//       const attributes = [
+//         {
+//           name: 'ASSET_NAME.ASSET_TYPE',
+//           value: 'ASSET_URI',
+//           type: 'uri',
+//         },
+//         {
+//           name: 'ASSET_NAME.ASSET_TYPE',
+//           value: 'ASSET_IPFS_URI',
+//           type: 'application/json',
+//         },
+//         {
+//           name: 'ASSET_NAME.manifest',
+//           value: 'ASSET_DEFINITION_URI',
+//           type: 'uri',
+//         },
+//         {
+//           name: 'ASSET_NAME.manifest',
+//           value: 'ASSET_MANIFEST_IFPS_URI',
+//           type: 'application/json',
+//         },
+//       ]
 
-      const expected = 'ASSET_IPFS_URI'
+//       const expected = 'ASSET_IPFS_URI'
 
-      const result = SUT.extractAttributesUri(attributes)
+//       const result = SUT.extractAttributesUri(attributes)
 
-      expect(result).toEqual(expected)
-    })
-  })
-})
+//       expect(result).toEqual(expected)
+//     })
+//   })
+// })

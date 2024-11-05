4d605b569b678fff58c888848db60194b8280632
Jeroen Branje <info@jeroenbranje.nl>
2024-11-05 12:14:05 +0100

    feat: asset detail with attributes
    Signed-off-by: Jeroen Branje <info@jeroenbranje.nl>

--- apps/envited.ascs.digital/common/types/index.ts
+++ apps/envited.ascs.digital/common/types/index.ts
@@ -1,2 +1,13 @@
 export { Language, AssetStatus, ButtonType, Columns, Size, ColorScheme, Role, CredentialType, FileType } from './types'
-export type { Action, Asset, AssetMetadata, Obj, User, Session, Profile, Environment, Token, TokenAttributes } from './types'
+export type {
+  Action,
+  Asset,
+  AssetMetadata,
+  Obj,
+  User,
+  Session,
+  Profile,
+  Environment,
+  Token,
+  TokenAttributes,
+} from './types'

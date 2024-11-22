f4af5dd3d845f641030b15ae4077648b967b0954
Roy Scheeren <hi@royscheeren.com>
2024-11-22 19:08:26 +0100

    refactor(envited.ascs.digital): debug
    Signed-off-by: Roy Scheeren <hi@royscheeren.com>

--- apps/envited.ascs.digital/common/asset/validateAndCreateMetadata.utils.ts
+++ apps/envited.ascs.digital/common/asset/validateAndCreateMetadata.utils.ts
@@ -27,7 +27,7 @@ export const _createFilename =
   ({ raw, sha256, CID }: { raw: any; sha256: Hasher<'sha2-256', 18>; CID: any }) =>
   async (byteArray: any) => {
     try {
-      console.log(typeof byteArray, instanceof byteArray)
+      console.log(typeof byteArray, byteArray instanceof Uint8Array)
       const rawBytes = raw.encode(byteArray)
       const hash = await sha256.digest(rawBytes)
       const cid = CID.create(1, raw.code, hash)

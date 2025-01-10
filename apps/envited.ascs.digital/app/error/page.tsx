f4016335101594ac3579a4693fd99f1d9dce5326
Roy Scheeren <hi@royscheeren.com>
2025-01-10 15:26:05 +0100

    refactor(envited.ascs.digital): update to next 14.2.23
    Signed-off-by: Roy Scheeren <hi@royscheeren.com>

--- apps/envited.ascs.digital/app/error/page.tsx
+++ apps/envited.ascs.digital/app/error/page.tsx
@@ -1,10 +1,19 @@
 'use client'
 
 import { useSearchParams } from 'next/navigation'
+import { Suspense } from 'react'
 
 import { Error } from '../../modules/Error'
 
 export default function Index() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ErrorBoundary />
+    </Suspense>
+  )
+}
+
+function ErrorBoundary() {
   const searchParams = useSearchParams()
   const error = searchParams.get('error')
 

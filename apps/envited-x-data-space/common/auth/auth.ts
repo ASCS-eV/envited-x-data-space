91e4639c3fb236d4631436556486eccdcc759cdf
Roy Scheeren <hi@royscheeren.com>
2025-04-02 13:24:33 +0200

    refactor: change pkh to did
    Signed-off-by: Roy Scheeren <hi@royscheeren.com>

--- apps/envited-x-data-space/common/auth/auth.ts
+++ apps/envited-x-data-space/common/auth/auth.ts
@@ -79,7 +79,7 @@ export const authOptions: NextAuthOptions = {
       profile: async profile => {
         return {
           id: profile.sub,
-          pkh: profile.sub,
+          did: profile.sub,
         }
       },
     },
@@ -174,7 +174,7 @@ export const authOptions: NextAuthOptions = {
       if (user) {
         token.user = user
       }
-      console.log('jwt profile', profile)
+      console.log('profile', profile)
       if (profile && profile.sub) {
         const { sub } = profile
         const connection = await db()
@@ -194,7 +194,6 @@ export const authOptions: NextAuthOptions = {
     },
     async session({ session, token }) {
       log.info('Building session')
-      log.info('token', token)
       if (session?.user) {
         session.user.did = token.user.did
         session.user.role = token.user.role

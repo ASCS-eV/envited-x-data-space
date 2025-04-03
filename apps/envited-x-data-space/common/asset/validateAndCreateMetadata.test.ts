6e8ccc4db21b0f68479beeb5f5e14fe55da83a20
Roy Scheeren <hi@royscheeren.com>
2025-04-03 15:21:13 +0200

    test: update test
    Signed-off-by: Roy Scheeren <hi@royscheeren.com>

--- apps/envited-x-data-space/common/asset/validateAndCreateMetadata.test.ts
+++ apps/envited-x-data-space/common/asset/validateAndCreateMetadata.test.ts
@@ -56,10 +56,8 @@ describe('common/asset/validateAndCreateMetadata', () => {
       const getUserByIssuerIdStub = jest
         .fn()
         .mockResolvedValue({ id: 'ISSUER_ID', addressGlobalIdentifierId: 'GLOBAL_IDENTIFIER_ID', name: 'NAME' }) as any
-      const getGlobalIdentifierByIdStub = jest
-        .fn()
-        .mockResolvedValue({ nss: 'ISSUER ADDRESS' }) as any
-    
+      const getGlobalIdentifierByIdStub = jest.fn().mockResolvedValue({ nss: 'ISSUER ADDRESS' }) as any
+
       const dbStub = jest.fn().mockResolvedValue({
         getUserById: getUserByIdStub,
         getUserByIssuerId: getUserByIssuerIdStub,

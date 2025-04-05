d1fc42e098f581031cddb0c6411b16dc48028cd1
Jeroen Branje <info@jeroenbranje.nl>
2025-04-05 06:21:14 +0200

    feat: clean up
    Signed-off-by: Jeroen Branje <info@jeroenbranje.nl>

--- apps/envited-x-data-space/common/http/http.ts
+++ apps/envited-x-data-space/common/http/http.ts
@@ -1,36 +1,11 @@
-import axios, { Axios, AxiosRequestConfig } from 'axios'
+import axios, { AxiosRequestConfig } from 'axios'
 
-export type UploadOptions = {
+type UploadOptions = {
   file: File
   url: string
   contentType: string
   onProgress: (percent: number) => void
 }
-/*
-export const _uploadFileWithOnProgress =
-  ({ axios }: { axios: Axios }) =>
-  async ({ file, url, contentType, onProgress }: UploadOptions): Promise<boolean> => {
-    try {
-      await axios.put(url, file, {
-        headers: {
-          'Content-Type': contentType,
-        },
-        onUploadProgress: event => {
-          if (event.total && onProgress) {
-            const percent = Math.round((event.loaded / event.total) * 100)
-            onProgress(percent)
-          }
-        },
-      })
-
-      return true
-    } catch {
-      return false
-    }
-  }
-
-export const uploadFileWithOnProgress = _uploadFileWithOnProgress({ axios })
-*/
 
 type HttpMethod = 'GET' | 'POST' | 'PUT'
 

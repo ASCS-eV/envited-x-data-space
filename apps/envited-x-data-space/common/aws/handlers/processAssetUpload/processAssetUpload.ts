6167aaa599de0b59540470eb9ca479a3aee5c00c
Roy Scheeren <hi@royscheeren.com>
2025-04-29 15:56:16 +0200

    feat: updat tzip metadata
    Signed-off-by: Roy Scheeren <hi@royscheeren.com>

--- apps/envited-x-data-space/common/aws/handlers/processAssetUpload/processAssetUpload.ts
+++ apps/envited-x-data-space/common/aws/handlers/processAssetUpload/processAssetUpload.ts
@@ -13,8 +13,9 @@ import {
   Manifest,
   MetadataType,
 } from '../../../asset/types'
-import { Asset, AssetMetadata, AssetStatus, User } from '../../../types'
+import { Asset, AssetMetadata, AssetStatus, GlobalIdentifier, User } from '../../../types'
 import { createTzip21Metadata } from '../../../tzip21/metadata'
+import { stringifyGlobalIdentifier } from '../../../globalIdentifiers'
 
 export const processAssetUpload =
   ({
@@ -79,11 +80,11 @@ export const processAssetUpload =
     predetermineCID: (array: Uint8Array) => Promise<string>
     getMinter: (asset: Asset) => Promise<User>
     streamToUint8Array: (stream: Readable) => Promise<Uint8Array>
-    extractManifest: (assetArchive: Uint8Array) => Promise<{ conforms: boolean; data: Manifest }>
+    extractManifest: (assetArchive: Uint8Array) => Promise<{ conforms: boolean; data: Manifest, cid: string, fileSize: number }>
     extractDomainMetadata: (
       assetArchive: Uint8Array,
       manifest: Manifest,
-    ) => Promise<{ conforms: boolean; data: Record<string, unknown>; cid: string }>
+    ) => Promise<{ conforms: boolean; data: Record<string, unknown>; cid: string, fileSize: number }>
     extractResources: (manifest: Manifest) => Record<string, unknown>
     extractFileFromArchive: (array: Uint8Array, path: string) => Promise<Readable>
     getCoverImage: (
@@ -140,7 +141,7 @@ export const processAssetUpload =
       // Validate uploaded asset
       const asset = await getAsset(Key)
       const assetCID = await predetermineCID(uploadedFile)
-      const { conforms: manifestConforms, data: manifest } = await extractManifest(uploadedFile)
+      const { conforms: manifestConforms, data: manifest, cid: manifestCID, fileSize: manifestFileSize } = await extractManifest(uploadedFile)
 
       if (!manifestConforms) {
         // Revert if validation fails
@@ -154,6 +155,7 @@ export const processAssetUpload =
         conforms: domainMetadataConforms,
         data: domainMetadata,
         cid: domainMetadataCID,
+        fileSize: domainMetadataFileSize,
       } = await extractDomainMetadata(uploadedFile, manifest)
       // Validate domain metadata
       if (!domainMetadataConforms) {
@@ -278,13 +280,19 @@ export const processAssetUpload =
         domainMetadata: {
           cid: domainMetadataCID,
           data: domainMetadata,
+          fileSize: domainMetadataFileSize,
         },
         manifest: {
-          cid: modifiedManifestCID,
-          fileSize: modifiedManifest.fileSize as number,
+          cid: manifestCID,
+          fileSize: manifestFileSize,
           data: manifest,
         },
-        minter: minter.addressGlobalIdentifier?.scopedIdentifier ?? '',
+        modifiedManifest: {
+          cid: modifiedManifestCID,
+          fileSize: modifiedManifest.fileSize as number,
+          data: modifiedManifest,
+        },
+        minter: stringifyGlobalIdentifier(minter.addressGlobalIdentifier as GlobalIdentifier) ?? '',
         rights: {
           identifier: pathOr('', MANIFEST_LICENSE)(manifest),
           path: pathOr('', MANIFEST_LICENSE_PATH)(manifest),
